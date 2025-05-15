630c154ff8b971fe9bd3b14697638ca559dadc39
htringuyen <tringuyenh75@gmail.com>
2025-05-15 17:02:18 +0700

    Update classficaiton apis

--- io.javaside.ts4e.core/src/main/java/io/javaside/ts4e/core/classification/ClassificationListener.java
+++ io.javaside.ts4e.core/src/main/java/io/javaside/ts4e/core/classification/ClassificationListener.java
@@ -1,4 +1,10 @@
 package io.javaside.ts4e.core.classification;
 
+import java.util.List;
+
 public interface ClassificationListener {
+
+    void initialize(List<ClassificationSpan> initSpans);
+
+    void classificationUpdated(List<ClassificationSpan> updatedSpans);
 }
--- io.javaside.ts4e.core/src/main/java/io/javaside/ts4e/core/classification/Classifier.java
+++ io.javaside.ts4e.core/src/main/java/io/javaside/ts4e/core/classification/Classifier.java
@@ -1,10 +1,12 @@
 package io.javaside.ts4e.core.classification;
 
+import io.javaside.ts4e.core.TSDocument;
+
 import java.util.List;
 
 public interface Classifier {
 
-    List<ClassificationSpan> getClassificationSpans();
+    void connect(TSDocument document);
 
     void addClassificationListener(ClassificationListener listener);
 }
